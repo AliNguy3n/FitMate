2bf58bbdc174057a380a128b725ea11ba8fdc164
AliNguy3n <linhnguyenduc.kts@gmail.com>
2025-07-22 00:03:29 +0700

    Fontend Admin Dashboard

---
+++ fontend_admin_full/Admin/src/helpers/real-backend.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+// interface UserData {
+//   id: number;
+//   username: string;
+//   password: string;
+//   firstName: string;
+//   lastName: string;
+//   role: string;
+//   token: string;
+// }
+
+// const mock = new MockAdapter(axios);
+
+export function configureRealBackend() {
+  
+}
